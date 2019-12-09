d586d0e43b1e79d483739f2fc35d36c0309c81a9
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-12-09 16:38:09 -0500

    minimal type-matching process

--- my_type.php
+++ my_type.php
@@ -1,16 +1,43 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
-session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-  header('Location: index.html');
-  exit();
-}
-// Include config
+// Include config file
 require_once "config.php";
 
-$query = 'SELECT type, description FROM types';
-$result = mysqli_fetch_all($con->query($query), MYSQLI_ASSOC);
+foreach ($_POST as $key => $value){
+
+  if ($value == "Tend to Agree" ){$choice_id = $key*2;}
+  else if ($value == "Tend to Disagree" ){$choice_id = $key*2+1;}
+    
+  if ($stmt = $con->prepare('
+      INSERT INTO chooses (account_id, choice_id) 
+      VALUES (?, ?)')){
+    $stmt->bind_param('ii', $id, $choice_id);
+    $stmt->execute();
+  } else { die ('Something went wrong!'); }
+  $stmt->close();
+}
+
+$stmt = $con->prepare('
+    SELECT x.type_id type_id
+    FROM choices x
+    JOIN (
+      SELECT choice_id 
+      FROM chooses 
+      WHERE account_id = ? 
+      ORDER BY choose_id DESC 
+      LIMIT 8) o
+    ON x.choice_id = o.choice_id');
+$stmt->bind_param('i', $id);
+$stmt->execute();
+$result = $stmt->get_result();
+while($row_data = $result->fetch_assoc()) {
+  $score += $row_data['type_id'];
+}
+
+/* Find Type from score */
+if ($score < 24)      {$type = 2;}
+else if ($score < 28) {$type = 3;}
+else if ($score < 32) {$type = 4;}
+else                  {$type = 5;} 
 ?>
 
 <!DOCTYPE html>
--- quiz.php
+++ quiz.php
@@ -1,13 +1,9 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
-session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-  header('Location: index.html');
-  exit();
-}
+
 // Include config
 require_once "config.php";
+
+// add in user form
 
 $query = 'SELECT question FROM questions';
 $result = mysqli_fetch_all($con->query($query), MYSQLI_ASSOC);
@@ -31,30 +27,34 @@ $result = mysqli_fetch_all($con->query($query), MYSQLI_ASSOC);
         <div class="hero">
         </div>
       </header>
-
-      <h2 id="qpage">Personality Quiz To Find Your Match!</h2>
+      <h2 id="qpage">Find Your match with this personality quiz!</h2>
       <div>
-	<form action="entry.php" method="post" autocomplete="off"> 
-	  <?php for ( $i=0; $i < 8; $i++ ){ ?>
+	      <form action="entry.php" method="post" autocomplete="off"> 
+	        <?php for ( $i=0; $i < 8; $i++ ){ ?>
             <blockquote>
               <?php echo $result[$i]['question']; ?>
               <select type="text" name="<?php echo $i+1 ?>" id="question">
-	        <option id="agree">Tend to Agree</option>
-	        <option id="disagree">Tend to Disagree</option>
+	              <option id="agree">Tend to Agree</option>
+	              <option id="disagree">Tend to Disagree</option>
               </select>
             </blockquote>
-          <?php } ?>
+            <?php } ?>
+            <div>
+              <input type="text" name="fname" placeholder="First Name" id="fname" required>
+	          </div>
+            <div>
+              <input type="text" name="lname" placeholder="Last Name" id="lname" required>
+	          </div>
+	          <div>
+	            <input type="email" name="email" placeholder="Email" id="email" required>
+	          </div>
           <input type="submit" value="submit" >
         </form>
       </div>
 
       <nav>
         <ul>
-          <li><a class="navigation" href="logout.php">LOGOUT</a></li>
           <li><a class="navigation" href="quiz.php">TAKE THE QUIZ!</a></li>
-          <li><a class="navigation" href="profile.php">PROFILE</a></li>
-	  <li><a class="navigation" href="index.html">LOGIN</a></li>
-	  <li><a class="navigation" href="register.html">REGISTER</a></li>
         </ul>
       </nav>
 
